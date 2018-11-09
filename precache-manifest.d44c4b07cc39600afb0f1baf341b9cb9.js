27d929542f8d2ed29af7d08a106c10465b23cb56
Jinjia Lin <linjj93@hotmail.com>
2018-11-09 21:28:47 +0800

    Updates

---
+++ precache-manifest.d44c4b07cc39600afb0f1baf341b9cb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32683e394aaac57a83f9",
+    "url": "/my-react-calculator/static/css/main.4fdd2ccd.chunk.css"
+  },
+  {
+    "revision": "32683e394aaac57a83f9",
+    "url": "/my-react-calculator/static/js/main.32683e39.chunk.js"
+  },
+  {
+    "revision": "a1f2bd7668b1c6d250da",
+    "url": "/my-react-calculator/static/js/1.a1f2bd76.chunk.js"
+  },
+  {
+    "revision": "3df9eddf9bfe3db7e011",
+    "url": "/my-react-calculator/static/js/runtime~main.3df9eddf.js"
+  },
+  {
+    "revision": "0ceee5530267a3be0c3270fb251a2832",
+    "url": "/my-react-calculator/index.html"
+  }
+];
