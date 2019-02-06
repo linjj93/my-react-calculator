98dd3229fd6670262c2023a2b620e12a11cc3f22
Jinjia Lin <linjj93@hotmail.com>
2019-02-06 13:04:44 +0800

    Updates

---
+++ precache-manifest.4d808706907f8741f2596cef08943b6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3df9eddf9bfe3db7e011",
+    "url": "/my-react-calculator/static/js/runtime~main.3df9eddf.js"
+  },
+  {
+    "revision": "c773fbd05aa92948e40a",
+    "url": "/my-react-calculator/static/js/main.c773fbd0.chunk.js"
+  },
+  {
+    "revision": "a1f2bd7668b1c6d250da",
+    "url": "/my-react-calculator/static/js/1.a1f2bd76.chunk.js"
+  },
+  {
+    "revision": "c773fbd05aa92948e40a",
+    "url": "/my-react-calculator/static/css/main.fb158339.chunk.css"
+  },
+  {
+    "revision": "a12edf4d3c25a0f18380bb8de0bcce20",
+    "url": "/my-react-calculator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-react-calculator/precache-manifest.0d4e40472fad0486488ef0f257763b1d.js"
+  "/my-react-calculator/precache-manifest.4d808706907f8741f2596cef08943b6b.js"
 );
 
 workbox.clientsClaim();
